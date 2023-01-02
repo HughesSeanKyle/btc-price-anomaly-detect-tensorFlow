40bf229859cd1c997a100e39dc6217410bceeb9c
HughesSeanKyle <khughessean@yahoo.com>
2023-01-02 20:22:46 +0200

    first commit

---
+++ src/components/Charts/LineChart.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-datalabels';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+const data = {
+	labels: [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sept',
+		'Oct',
+		'Nov',
+		'Dec',
+	],
+	datasets: [
+		{
+			label: 'Price',
+			data: [
+				6500, 5900, 8000, 8100, 5600, 5500, 4000, 8000, 8100, 5600, 6500, 5900,
+				8000,
+			],
+			borderColor: '#3e95cd',
+			fill: false,
+		},
+	],
+};
+
+const options = {
+	legend: {
+		display: false,
+	},
+	scales: {
+		yAxes: [
+			{
+				scaleLabel: {
+					display: true,
+					labelString: 'Price',
+				},
+			},
+		],
+		xAxes: [
+			{
+				scaleLabel: {
+					display: true,
+					labelString: 'Date',
+				},
+			},
+		],
+	},
+};
+
+const LineChart = () => {
+	return <Line data={data} options={options} />;
+};
+
+export default LineChart;
